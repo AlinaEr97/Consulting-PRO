a914a1836ab19b5969dcb00cc301fc9c1bf078a8
AlinaEr97 <86489788+AlinaEr97@users.noreply.github.com>
2022-06-06 19:31:54 +0300

    Учтены замечания валидаторы

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,8 @@
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll('.lock-padding');
 const curentPopup = document.querySelector('.popup__content');
 
 let unlock = true;
-
-
 
 if (popupLinks.length > 0) {
 	for (let i = 0; i < popupLinks.length; i++) {
@@ -36,60 +33,13 @@ function popupOpen(curentPopup) {
 		if (popupActive) {
 			popupClose (popupActive, false);
 		}
-		else { 
-			bodyLock();
-		}
 		curentPopup.classList.add('popup-open');
-		curentPopup.addEventListener("click", function(e) {
-			if (!e.target.closest('.popup__content')) {
-				popupClose(e.target.closest('.popup'));
-			}
-		});
 	}
 }
-function popupClose(popupActive, doUnlock = true) {
+function popupClose(popupActive = true) {
 	if (unlock) {
 		popupActive.classList.remove('popup-open');
-		if (doUnlock) {
-			bodyUnlock();
-		}
 	}
-}
-
-function bodyLock() {
-	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-
-	if (lockPadding.length > 0) {
-		for (let i = 0; i < lockPadding.length; i++) {
-			const el = lockPadding[i];
-			el.style.paddingRight = lockPaddingValue;
-		}
-	}
-	body.style.paddingRight = lockPaddingValue;
-	body.classList.add('lock');
-
-	unclock = false;
-	setTimeout(function () {
-		unlock = true;
-	});
-}
-
-function bodyUnlock() {
-	setTimeout(function () {
-		if (lockPadding.length > 0) {
-			for (let i = 0; i < lockPadding.length; i++) {
-				const el = lockPadding[i];
-				el.style.paddingRight = '0px';
-			}
-		}
-		body.style.paddingRight = '0px';
-		body.classList.remove('lock');
-	});
-
-	unlock = false;
-	setTimeout(function() {
-		unlock = true;
-	});
 }
 
 document.addEventListener('keydown', function (e) {
